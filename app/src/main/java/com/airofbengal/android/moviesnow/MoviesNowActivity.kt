af17dc6e4a1b34c1411a523b2f0d8a1a482646a7
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2020-12-19 11:31:44 +0600

    Initial commit

---
+++ app/src/main/java/com/airofbengal/android/moviesnow/MoviesNowActivity.kt
@@ -0,0 +1,11 @@
+package com.airofbengal.android.moviesnow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoviesNowActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movies_now)
+    }
+}
